9ed33ca1b110721dc67f78fffb10e8dd472edf2c
samanta <samanta-07@hotmail.com>
2019-05-03 14:35:42 +0200

    added form controller, configured azure dev spaces for SimpleCalculatorClient for making calls to SimpleCalculator Service.

---
+++ SimpleCalculatorClient/Controllers/FormController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleCalculatorClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SimpleCalculatorClient.Controllers
+{
+    [Route("api/[controller]")]
+    public class FormController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(FormModel model)
+        {
+            using (var client = new HttpClient())
+            {
+                // Call *mywebapi*, and display its response in the page
+                var request = new HttpRequestMessage();
+                var uri = new Uri($"http://default.simplecalculator.z26kl7w9th.weu.azds.io/api/calculator/add?x={model.NumbOne}&y={model.NumbTwo}");
+                request.RequestUri = uri;
+                if (this.Request.Headers.ContainsKey("azds-route-as"))
+                {
+                    // Propagate the dev space routing header
+                    request.Headers.Add("azds-route-as", this.Request.Headers["azds-route-as"] as IEnumerable<string>);
+                }
+                var response = await client.SendAsync(request);
+                var content = await response.Content.ReadAsStringAsync();
+                ViewData["Message"] += " and " + content;
+            }
+
+            return Content($"First: {model.NumbOne}\nSecond: {model.NumbTwo}");
+        }
+    }
+}
---
+++ SimpleCalculatorClient/Models/FormModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleCalculatorClient.Models
+{
+    public class FormModel
+    {
+        public int NumbOne { get; set; }
+        public int NumbTwo { get; set; }
+    }
+}
